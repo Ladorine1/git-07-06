cc4b9cb2d02708d7990ef4500066674eceaba450
Ladorine1 <ladorine.youogo@live.fr>
2023-06-19 21:06:22 +0000

    change done from dev branch

--- first-code.py
+++ first-code.py
@@ -4,6 +4,5 @@ print ("Current date and time : ")
 print (now.strftime("%Y-%m-%d %H:%M:%S"))
 print ("Hello guys !! learning git ")
 print ("Hey friends !! come learn with me")
-<<<<<<< HEAD
 print (" I'm learning GIT on AWS on local and remote repo")
-
+print ("change done on dev branch")
